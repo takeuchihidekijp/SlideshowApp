d218e6431c4e1bf6378106dceaa1cf78aeb16afe
takeuchihidekijp <takeuchihidekijp@yahoo.co.jp>
2017-10-03 20:19:28 +0900

    kauai

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -37,7 +37,7 @@ class ViewController: UIViewController {
     @IBAction func slideShow(_ sender: Any) {
         
         if self.timer == nil {
-           self.timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+           self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
             
             go_btn.isEnabled = false
             backbtn.isEnabled = false
